dc022230a2b5ed01372f3abf4406e31d60402a7c
Calvine69 <Clvn.70@gmail.com>
2025-04-16 15:30:24 +0700

    Update talent material model

--- models/talentMaterial.js
+++ models/talentMaterial.js
@@ -1,18 +1,15 @@
-const mongoose = require('mongoose');
+// models/talentMaterial.js
+const mongoose = require("mongoose");
 
 const TalentMaterialSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  iconUrl: {
-    type: String,
-    required: true
-  },
-  characters: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Character'
-  }]
+  name: String,
+  iconUrl: String,
+  characters: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Character"
+    }
+  ]
 });
 
-module.exports = mongoose.model('TalentMaterial', TalentMaterialSchema);
+module.exports = mongoose.model("TalentMaterial", TalentMaterialSchema);
