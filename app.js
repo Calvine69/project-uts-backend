ce0d3f23a3e9d835f60a9ae698c72624230c781f
cuang12 <darren5tjuang@gmail.com>
2025-04-16 12:22:03 +0700

    Update app.js
    nambahin global error dan 404 route not found

--- app.js
+++ app.js
@@ -34,6 +34,17 @@ app.get("/", (req, res) => {
   res.send("🌟 Welcome to the Genshin API Clone!");
 });
 
+// 404 Not Found Handler
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Route not found" });
+});
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error" });
+});
+
 // Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
