74d0b9836e384b980fa989390f9f6378ada94fec
Julio tevez <tevezcool123@gmail.com>
2025-04-16 12:02:58 +0700

    Update talentMaterial.js

---
+++ models/talentMaterial.js
@@ -0,0 +1,15 @@
+{
+    "characters": [
+        {
+        "_id": "60a4d3b193b76f18901aae9d",
+        "name": "Yanfei",
+        "weaponType": "Catalyst",
+        "rarity": 4,
+        "cardImageURL": "https://res.cloudinary.com/dnoibyqq2/image/upload/v1617899636/genshin-app/characters/yanfei/card.jpg"
+        }
+    ],
+    "_id": "60a4d22e984c49383c6ae0d0",
+    "name": "Bloodjade Branch",
+    "iconUrl": "https://res.cloudinary.com/dnoibyqq2/image/upload/v1620824560/genshin-app/talent-boss-items/tail-of-boreas.png",
+    "__v": 0
+}
