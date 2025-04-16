19ab51af10694a0baaa04154634b6f1fcd732a4f
Calvine69 <Clvn.70@gmail.com>
2025-04-16 15:28:15 +0700

    Update talent material model

--- models/talentMaterial.js
+++ models/talentMaterial.js
@@ -1,14 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const CharacterSchema = new mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId, // The character's _id (as an ObjectId)
-  name: String,
-  weaponType: String,
-  rarity: Number,
-  cardImageURL: String
-});
-
-const TalentBookSchema = new mongoose.Schema({
+const TalentMaterialSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
@@ -17,15 +9,10 @@ const TalentBookSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  rarity: {
-    type: Number,
-    required: true
-  },
-  farmingDays: {
-    type: [String],
-    required: true
-  },
-  characters: [CharacterSchema]
+  characters: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Character'
+  }]
 });
 
-module.exports = mongoose.model("TalentBook", TalentBookSchema);
+module.exports = mongoose.model('TalentMaterial', TalentMaterialSchema);
