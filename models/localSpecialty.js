c6667d6a544c72149ed0175ae2b092381cc03f9d
Calvine69 <Clvn.70@gmail.com>
2025-04-16 15:24:39 +0700

    Update Model local Specialty

--- models/localSpecialty.js
+++ models/localSpecialty.js
@@ -1,20 +1,15 @@
-[
+const mongoose = require("mongoose");
+
+const LocalSpecialtySchema = new mongoose.Schema({
+  name: String,
+  iconUrl: String,
+  sources: [String],
+  characters: [
     {
-        "sources": [
-            "Found in the wild",
-            "Found in Springvale",
-            "Sold by Flora"
-        ],
-        "characters": [
-            "609f738f03adb03e6c971995",
-            "609f73b903adb03e6c97199a"
-        ],
-        "_id": "60a37fc762b1d338900ae6e9",
-        "name": "Calla Lily",
-        "iconUrl": "https://res.cloudinary.com/dnoibyqq2/image/upload/v1620824986/genshin-app/specialties/calla-lily.png",
-        "__v": 0
-    },
-    {
-        ...
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Character"
     }
- ]
+  ]
+});
+
+module.exports = mongoose.model("LocalSpecialty", LocalSpecialtySchema);
