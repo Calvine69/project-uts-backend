cbf4d6d67ac8ed06f19544c264ea1cf929f96e2a
Yudhistira Jovian I <jojovian2006@gmail.com>
2025-04-16 12:26:49 +0700

    Update localSpecialty.js

---
+++ models/localSpecialty.js
@@ -0,0 +1,20 @@
+[
+    {
+        "sources": [
+            "Found in the wild",
+            "Found in Springvale",
+            "Sold by Flora"
+        ],
+        "characters": [
+            "609f738f03adb03e6c971995",
+            "609f73b903adb03e6c97199a"
+        ],
+        "_id": "60a37fc762b1d338900ae6e9",
+        "name": "Calla Lily",
+        "iconUrl": "https://res.cloudinary.com/dnoibyqq2/image/upload/v1620824986/genshin-app/specialties/calla-lily.png",
+        "__v": 0
+    },
+    {
+        ...
+    }
+ ]
