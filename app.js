5721fb11e27b54e6303b5aed243179f6dab6e681
Calvine69 <Clvn.70@gmail.com>
2025-04-18 17:40:22 +0700

    Update app.js menambahkan route character

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 // Importing dependencies
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config(); // Untuk menggunakan variabel dari .env
+require('dotenv').config();
 
 // Inisialisasi Express
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// MongoDB URI dari .env
+// MongoDB URI
 const mongoURI = process.env.DB_CONNECTION;
-
-// Koneksi ke MongoDB menggunakan mongoose
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Successfully connected to MongoDB');
-  })
-  .catch((err) => {
-    console.error('MongoDB connection error:', err);
-  });
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
 
 // Middleware
 app.use(express.json());
 
-// Route untuk root URL '/'
+// Tambahkan routing karakter (gunakan /characters sebagai base path)
+const characterRoutes = require('./routes/characterRoutes');
+app.use('/characters', characterRoutes);
+
+// Route default
 app.get('/', (req, res) => {
   res.send('Welcome to the API! Successfully connected to MongoDB.');
 });
 
-// Menjalankan server
+// Jalankan server
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
