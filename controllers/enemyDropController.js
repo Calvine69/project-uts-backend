bff93c473e6f3cbcb924479720782f7415b001db
Calvine69 <Clvn.70@gmail.com>
2025-04-18 18:30:16 +0700

    mengupdate enemydropcontroller.js

--- controllers/enemyDropController.js
+++ controllers/enemyDropController.js
@@ -1,26 +1,41 @@
-const service = require("../services/enemyDropService");
+const EnemyDrop = require('../models/enemyDrop');
 
-const getAll = async (req, res) => {
+// POST satu data enemy drop
+exports.createEnemyDrop = async (req, res) => {
   try {
-    const items = await service.fetchAll();
-    res.json(items);
+    const newDrop = await EnemyDrop.create(req.body);
+    res.status(201).json(newDrop);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// POST banyak data enemy drop
+exports.createManyEnemyDrops = async (req, res) => {
+  try {
+    const newDrops = await EnemyDrop.insertMany(req.body);
+    res.status(201).json(newDrops);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// GET semua enemy drop
+exports.getAllEnemyDrops = async (req, res) => {
+  try {
+    const drops = await EnemyDrop.find();
+    res.status(200).json(drops);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 };
 
-const getByName = async (req, res) => {
+// DELETE semua enemy drop
+exports.deleteAllEnemyDrops = async (req, res) => {
   try {
-    const name = req.params.name;
-    const item = await service.fetchByName(name);
-    if (!item) return res.status(404).json({ message: "Enemy Drop not found" });
-    res.json(item);
+    await EnemyDrop.deleteMany({});
+    res.status(200).json({ message: 'Semua enemy drop berhasil dihapus.' });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
-};
-
-module.exports = {
-  getAll,
-  getByName
 };
