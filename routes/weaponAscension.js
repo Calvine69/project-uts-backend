181f1529ccf377a9d614f8293970d1afdc4b81cb
AndiGautama <andigautama010@gmail.com>
2025-04-19 22:40:57 +0700

    menambah weaponascension

--- routes/weaponAscension.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/weaponAscensionController");
-
-router.get("/", controller.getAll);
-router.get("/:name", controller.getByName);
-
-module.exports = router;
